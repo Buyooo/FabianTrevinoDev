2998e4e0f2f72b3c85a2c16da4b9f32502dfbeb7
Fabian T <19524112+Buyooo@users.noreply.github.com>
2021-10-22 23:29:48 -0500

    Fix initial violations

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,14 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FabianTrevinoDev.Pages
 {
-    public class IndexModel : PageModel
+	public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
